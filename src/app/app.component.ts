c06a4918e078758ca35ec140c4cdf074c3cbe452
Xinito159 <yo_registro@ymail.com>
2024-02-17 00:43:41 +0100

    feat: integrar estilos al wallet adapter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
 
 @Component({
   standalone: true,
-  imports: [ RouterModule],
+  imports: [ RouterModule, HdWalletMultiButtonComponent],
   selector: 'dapp1-root',
   template: `
-    <header>
-      <h1> Hola Soy Xinitono.</h1>
+    <header class="py-8">
+      <h1 class="text-2xl text-center mb-4"> Hola Soy Xinitono.</h1>
+
+      <div class="flex justify-center">
+      <hd-wallet-multi-button></hd-wallet-multi-button>
+      </div>
     </header>
   `
 })
