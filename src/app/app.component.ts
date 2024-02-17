a65fcf3d830014c75974f505ad3ff060d7ed27e4
Xinito159 <yo_registro@ymail.com>
2024-02-17 02:40:17 +0100

    feat: agregamos dos páginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import { ShyftApiService } from './shyft-api.service';
 import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { computedAsync } from 'ngxtension/computed-async';
+import { MatAnchor } from '@angular/material/button'
+
 
 @Component({
   standalone: true,
-  imports: [ RouterModule, HdWalletMultiButtonComponent],
+  imports: [ RouterModule, HdWalletMultiButtonComponent, MatAnchor],
   selector: 'dapp1-root',
   template: `
     <header class="py-8">
@@ -19,12 +21,27 @@ import { computedAsync } from 'ngxtension/computed-async';
       </div>
 
       @if (account()){
-      <div class="flex justify-center items-center py-8">
+      <div class="flex justify-center items-center py-4">
         <img [src]="account()?.info?.image" class="w-8 h-8"/>
         <p class="text-xl">{{ account()?.balance}}</p>
       </div>      
       }
+
+      <nav>
+        <ul class="flex justify-center items-center gap-4 py-4">
+          <li>
+            <a [routerLink]="['']" mat-raised-button>Home</a>
+          </li>
+          <li>
+            <a [routerLink]="['settings']" mat-raised-button>Settings</a>
+          </li>
+        </ul>
+      </nav>
     </header>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `
 })
 export class AppComponent {
---
+++ src/app/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'dapp1-home-page',
+    template: `
+        <h2 class="text-2xl text-center mb-4"> Home</h2>
+        ` 
+})
+export class HomePageComponent {
+}
